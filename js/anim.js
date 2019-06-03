3e69ac48c01088c80e03b67a701ed15542d9c277
razik <ellifaz.marciano@outlook.com>
2019-06-03 14:34:23 -0300

    termino do footer

--- js/anim.js
+++ js/anim.js
@@ -1,1 +1,21 @@
-AOS.init();
+$(function () {
+    $('.sidenav').sidenav();
+
+    $('.scrollspy').scrollSpy({
+        scrollOffset: 100
+    });
+
+    $('.materialboxed').materialbox();
+
+    $('.sidenav li').click(() => {
+        $('.sidenav').sidenav('close');
+    });
+
+    $(window).on('scroll', function() {
+        if($(window).scrollTop() > 100) {
+            $('.navbar').addClass('nav-color');
+        } else {
+            $('.navbar').removeClass('nav-color');
+        }
+    });
+});
